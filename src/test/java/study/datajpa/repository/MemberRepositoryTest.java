bdb19a88183f93e4778d80a8df8484eba6a39759
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2022-12-27 12:03:46 +0900

    메소드 이름으로 쿼리 생성

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -56,4 +56,30 @@ class MemberRepositoryTest {
         long deletedCount = memberRepository.count();
         assertThat(deletedCount).isEqualTo(0);
     }
+
+    @Test
+    public void findByUsernameAndAgeGreaterThan() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+        List<Member> result =
+                memberRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
+        assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+        assertThat(result.get(0).getAge()).isEqualTo(20);
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void findTestBy() {
+        Member u1 = new Member("u1", 10);
+        Member u2 = new Member("u2", 20);
+        memberRepository.save(u1);
+        memberRepository.save(u2);
+        
+        List<Member> testBy = memberRepository.findTestBy(); //전체 조회
+        for (Member member : testBy) {
+            System.out.println("member = " + member);
+        }
+    }
 }
